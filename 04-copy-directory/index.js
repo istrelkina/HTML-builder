af47aea629937721dad96f70eaf16ab09127c34c
istrelkina <istrelkina@gmail.com>
2022-05-19 21:24:47 +1000

    fix: add relative path 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,31 +3,44 @@ const fs = require('fs')
 const path = require('path')
 let files;
 
-async function getFiles() {
+async function copyFiles(pathSrc, pathDest, dir) {
+   makeDir(pathDest, dir)
    try {
-      files = await readdir(path.join(__dirname, 'files'), 'utf8', true)
-      copyFile(files);
+      files = await readdir(path.join(pathSrc, dir), 'utf8', true)
+      for (const file of files) {
+         fs.stat(path.join(pathSrc, dir, file), (err, stats) => {
+            if (err) throw err;
+            if (stats.isFile()) {
+               const newDir = dir + '-copy';
+               copyFile(path.join(pathSrc, dir), path.join(pathDest, newDir), file);
+            } else {
+               const newDir = dir + '-copy';
+               copyFiles(path.join(pathSrc, dir), path.join(pathDest, newDir), file)
+            }
+         });
+
+      }
    } catch (err) {
       console.log('Что-то пошло не так с файлами')
    }
 }
 
-async function checkDir(pathFolder) {
+async function makeDir(pathFolder, dir) {
    try {
-      const dir = await fs.promises.mkdir(pathFolder, true)
+      const newDir = dir + '-copy';
+      await fs.promises.mkdir(path.join(pathFolder, newDir), true)
    } catch (err) {
       //console.log('Ошибка ', err)
    }
 
 }
-async function copyFile(files) {
+async function copyFile(pathSrc, pathDest, file) {
    try {
-      for (const file of files)
-         await fs.promises.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file));
+      await fs.promises.copyFile(path.join(pathSrc, file), path.join(pathDest, file));
    } catch (err) {
       console.log(err);
    }
 }
-checkDir(path.join(__dirname, 'files-copy'))
-getFiles();
+
+copyFiles(__dirname, __dirname, 'files');
 
