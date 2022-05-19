688c089a86d3329a60e49a22ab8a659a442cc4c4
istrelkina <istrelkina@gmail.com>
2022-05-19 19:38:15 +1000

    feat: add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const { readdir } = require('fs/promises')
+const fs = require('fs')
+const path = require('path')
+let files;
+
+async function getFiles() {
+   try {
+      files = await readdir(path.join(__dirname, 'files'), 'utf8', true)
+      copyFile(files);
+   } catch (err) {
+      console.log('Что-то пошло не так с файлами')
+   }
+}
+
+async function checkDir(pathFolder) {
+   try {
+      const dir = await fs.promises.mkdir(pathFolder, true)
+   } catch (err) {
+      //console.log('Ошибка ', err)
+   }
+
+}
+async function copyFile(files) {
+   try {
+      for (const file of files)
+         await fs.promises.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file));
+   } catch (err) {
+      console.log(err);
+   }
+}
+checkDir(path.join(__dirname, 'files-copy'))
+getFiles();
 
